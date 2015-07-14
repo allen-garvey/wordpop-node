08007b9aefb70830fb4c4524dca904fc6f866387
Allen Garvey <allengarvey@hotmail.com>
2015-07-14 10:45:46 -0400

    update gulpfile to build scripts in correct directory

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,21 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var maps = require('gulp-sourcemaps');
 
+var scriptsDir = 'public_html/scripts';
+
 gulp.task('concatScripts', function(){
 	return gulp.src(['js/counted_set.js', 'js/data_viz.js', 'js/app.js'])
 		.pipe(maps.init())
 		.pipe(concat('app.js'))
 		.pipe(maps.write('./'))
-		.pipe(gulp.dest('scripts'));
+		.pipe(gulp.dest(scriptsDir));
 });
 
 gulp.task('minifyScripts', ['concatScripts'], function(){
-	return gulp.src('scripts/app.js')
+	return gulp.src(scriptsDir + '/app.js')
 		.pipe(uglify())
 		.pipe(rename('app.min.js'))
-		.pipe(gulp.dest('scripts'));
+		.pipe(gulp.dest(scriptsDir));
 });
 gulp.task('watchScripts', function(){
 	gulp.watch('js/**/*.js', ['minifyScripts']);
