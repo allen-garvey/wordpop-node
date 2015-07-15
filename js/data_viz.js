cf88c29487e50d67f7f382639f36c7acda8a4a18
Allen Garvey <allengarvey@hotmail.com>
2015-07-14 22:10:49 -0400

    add dynamic scale for data visualization and make the viz wrapper the size of the window innerWidth and innerHeight

--- js/data_viz.js
+++ js/data_viz.js
@@ -1,7 +1,7 @@
 WDP.displayViz = function(data){
 	//height and width of svg
-	var height = 800;
-	var width = 500;
+	var height = window.innerHeight;
+	var width = window.innerWidth;
 	var padding = 0;
 
 	var viz = d3.select("#viz-wrapper")
@@ -10,8 +10,13 @@ WDP.displayViz = function(data){
 					.attr('height', height)
 					.attr('width', width);
 
+	var rDomain = d3.extent(data, function(d){
+        return d.amount;
+    });
+	var rScale = d3.scale.linear().domain(rDomain).range([1, 25]);
+
 	var dots = viz.selectAll('circle').data(data).enter().append('circle');
-	dots.attr('r', function(d){ return d.amount; })
+	dots.attr('r', function(d){ return rScale(d.amount); })
 		.attr('cx', function(d){return Math.max(0 + padding, Math.random() * width - padding)})
 		.attr('cy', function(d){return Math.max(0 + padding, Math.random() * height - padding)});
 }
