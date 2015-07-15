e4127e13cd4a6c717d7839d8cbcdc6c92e50c7bc
Allen Garvey <allengarvey@hotmail.com>
2015-07-14 23:42:45 -0400

    contain all circles and text in svg g element

--- js/data_viz.js
+++ js/data_viz.js
@@ -8,17 +8,40 @@ WDP.displayViz = function(data){
 					.append('svg')
 					.attr('id', 'viz')
 					.attr('height', height)
-					.attr('width', width);
+					.attr('width', width)
+					.append('g');
 
 	var rDomain = d3.extent(data, function(d){
         return d.amount;
     });
 	var rScale = d3.scale.linear().domain(rDomain).range([1, 25]);
 
-	var dots = viz.selectAll('circle').data(data).enter().append('circle');
-	dots.attr('r', function(d){ return rScale(d.amount); })
-		.attr('cx', function(d){return Math.max(0 + padding, Math.random() * width - padding)})
-		.attr('cy', function(d){return Math.max(0 + padding, Math.random() * height - padding)});
+	var dots = viz.selectAll('g.dots').data(data).enter().append('g').attr('class', 'dots');
+	dots.attr('transform', function(d){
+		var x = Math.max(padding, Math.random() * width - padding);
+		var y = Math.max(padding, Math.random() * height - padding);
+		return "translate(" + x + ", " + y + ")";
+	});
+
+	dots.append('circle').attr('r', function(d){ return rScale(d.amount); });
+	dots.append('text').text(function(d){return d.name;});
+
+	/*
+	//background rectangles are being created, but I cannot get them to show up for some reason
+	viz.selectAll("text").each(function(d, i){
+		var bbox = this.getBBox();
+		console.log(bbox);
+		var padding = 2;
+		var rect = d3.select(document.createElement("rect"))
+			.attr("x", bbox.x - padding)
+		    .attr("y", bbox.y - padding)
+		    .attr("width", bbox.width + (padding*2))
+		    .attr("height", bbox.height + (padding*2))
+		    .attr('class', 'word_label_background');
+		this.parentNode.insertBefore(rect.node(), this.nextSibling);
+
+	});
+	*/
 }
 
 
