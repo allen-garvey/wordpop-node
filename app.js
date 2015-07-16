82eada38369dd4a2ccf3aaf873c8ba5a0bbbdfbb
Allen Garvey <allengarvey@hotmail.com>
2015-07-16 01:11:31 -0400

    add routing to dynamically send craigslist page based on request

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 var http = require('http');
+var bodyParser = require('body-parser');
+var ajaxSearchHandler = require('./models/request_to_url');
 
 app.use(express.static('public_html'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 var responseOptions = {
     root: __dirname,
@@ -14,23 +18,29 @@ var responseOptions = {
 };
 
 app.get('/', function (req, res) {
-  res.sendFile('views/homepage.html', responseOptions);
+    res.sendFile('views/homepage.html', responseOptions);
 });
 
-app.get('/search/cl/web/?', function (req, res) {
-  http.get("http://newyork.craigslist.org/search/web", function(searchResponse) {
-        searchResponse.pipe(res);
-    });
+app.post('/data', function (req, res) {
+    console.log(req.body);
+    var reqUrl = ajaxSearchHandler.urlFromRequest(req.body);
+    if(reqUrl.match(/^http:/)){
+        http.get(reqUrl, function(searchResponse) {
+            searchResponse.pipe(res);
+        }); 
+    }
+    else{
+        res.status(400).send('400 Bad request');
+    }
 });
 
 //The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', function(req, res){
-  res.status(404).send('404 not found');
+    res.status(404).send('404 not found');
 });
 
 var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Wordpop-node app listening at http://%s:%s', host, port);
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Wordpop-node app listening at http://%s:%s', host, port);
 });
