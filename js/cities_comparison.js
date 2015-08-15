5a051e8b892d998f492f2f2d6a2d771dc0a7b2c0
Allen Garvey <allengarvey@hotmail.com>
2015-08-15 16:01:42 -0400

    add progress counter

--- js/cities_comparison.js
+++ js/cities_comparison.js
@@ -4,17 +4,16 @@
 WDP.cc = {}; //top level cities comparison container
 WDP.cc.count = {}; //container for how many pages to download
 WDP.cc.init = function(){
-	WDP.error.display('Gathering data...');
 	WDP.cc.set = new WDP.set.countedSet();
 	$.getJSON(WDP.baseUrl + 'data/cl.json', function(searchModel) {
 		var model = searchModel.craigslist;
 		WDP.models.cl = model;
 		var citiesLength = Object.keys(WDP.models.cl.cities).length;
-		var categoriesLength = 4;
-		WDP.cc.count.total = citiesLength * categoriesLength;
+		var categories = ['web', 'software', 'internet_engineering', 'systems_networking'];
+		WDP.cc.count.total = citiesLength * categories.length;
 		WDP.cc.count.done = 0;
 		$.each(Object.keys(WDP.models.cl.cities),function(index, city) {
-			$.each(['web', 'software', 'internet_engineering', 'systems_networking'],function(index, subcategory) {
+			$.each(categories,function(index, subcategory) {
 				WDP.getCLPage(new WDP.init.CraigslistRequestData(city, 'jobs',subcategory, null),function(searchResults, requestData){WDP.cc.getCityResults(searchResults, requestData);});
 			});
 		});
@@ -29,6 +28,7 @@ WDP.cc.getCityResults = function(searchResults, requestData){
 	var searchNum = $(searchResults).find('a.hdrlnk').length;
 	var key = WDP.models.cl.cities[requestData.city].display
 	WDP.cc.set.setCountForItem(key, WDP.cc.set.countForItem(key) + searchNum);
+	WDP.display.counter(WDP.cc.count.done, WDP.cc.count.total);
 	if(WDP.cc.count.done === WDP.cc.count.total){
 		WDP.error.clear();
 		WDP.cc.clearBarChart();
