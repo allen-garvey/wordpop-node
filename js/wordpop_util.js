72a19b3ed115cbfdbfc633aef4f6683b5ecb9d5b
Allen Garvey <allengarvey@hotmail.com>
2015-08-15 22:45:38 -0400

    add format number function for bar graph output

--- js/wordpop_util.js
+++ js/wordpop_util.js
@@ -1,7 +1,16 @@
+WDP.util = {};
 WDP.error = {};
 WDP.error.display = function(errorMsg){
 	document.getElementById('error_container').innerHTML = "<p>" + errorMsg + "</p>";
 }
 WDP.error.clear = function(){
 	document.getElementById('error_container').innerHTML = '';	
+}
+
+/*
+* Function returns a number formatted with commas as appropriate
+* adapted from: http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+*/
+WDP.util.formatNumber = function(n) {
+    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
